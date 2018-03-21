716edf666ff74d8f738fa450bb83de7dc9b570b6
Samuel SIlva <samuelsilvawb@gmail.com>
2018-03-21 12:38:32 -0300

    feat : user can change between celsius and fahrenheit
    - create logic to change values

--- src/js/app.js
+++ src/js/app.js
@@ -15,11 +15,31 @@
 		const $title = document.querySelector('.weather__content__title > h5');
 		const $temperature = document.querySelector('.weather__content__temperature > span');
 		const $description = document.querySelector('.weather__content__description');
+		const $typeTemperature = document.querySelector('#js-type-temperature');
 
 		$title.innerHTML = city;
 		$temperature.innerHTML = `${temp}°`;
 		$description.innerHTML =
 			`<span>${weather.description}</span><br>` +
-			(weather.icon ? `<img class="" src="${weather.icon}" alt="${weather.description}">` : '');
+			(weather.icon ? `<img src="${weather.icon}" alt="${weather.description}">` : '');
+
+		$typeTemperature.addEventListener('click', (e) => {
+			const text = e.srcElement;
+			if (text && text.textContent === 'C') {
+				text.textContent = 'F';
+				$temperature.textContent = celsiusToFahrenheit($temperature.textContent.replace(/°/, '')) + '°';
+			} else {
+				text.textContent = 'C';
+				$temperature.textContent = fahrenheitToCelsius($temperature.textContent.replace(/°/, '')) + '°';
+			}
+		})
+	}
+
+	function fahrenheitToCelsius(temperature) {
+		return Math.floor((temperature - 32) * (5 / 9));
+	}
+
+	function celsiusToFahrenheit(temperature) {
+		return Math.floor(temperature * 9 / 5 + 32);
 	}
 })();
